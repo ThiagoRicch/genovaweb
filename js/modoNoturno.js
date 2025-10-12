510b634a2059bf1e0f4368b11bda03ba9da68293
Thiago Nogueira <tigerthiago56@gmail.com>
2025-10-12 18:52:24 -0300

    algumas alterações

--- js/modoNoturno.js
+++ js/modoNoturno.js
@@ -8,12 +8,18 @@ class ThemeSwitcher {
     this.imgCardCerebro = document.querySelector('.imagem__card__cerebro');
     this.imgCardCerebro2 = document.querySelector('.imagem__card__cerebro2');
     this.imgCardBot = document.querySelector('.imagem__card__bot');
-    this.imgGenovaProfile = document.querySelector('.imagem__genova__profile'); // ✅ novo
-
+    this.imgGenovaProfile = document.querySelector('.imagem__genova__profile');
     this.titulo = document.querySelector('.titulo__menu__navbar');
     this.tituloSlideTablet = document.querySelector('.titulo__slide2__tablet');
-    this.paragrafoSlideTablet = document.querySelector('.paragrafo__slide2__tablet');
-
+    this.imgHomePage = document.querySelector('.img__home');
+    this.imgChatGenova = document.querySelector('.img__chat-genova');
+    this.imgOpenSupport = document.querySelector('.img__support');
+    this.imgMyProfile = document.querySelector('.img__profile');
+    this.imgHistoryIA = document.querySelector('.img__history-ia');
+    this.imgHistoryChamado = document.querySelector('.img__history-chamado');
+    this.imgFaleConosco = document.querySelector('.img__fale-conosco');
+    this.imgTheme = document.querySelector('.img__theme');
+    
     this.initEvents();
   }
 
@@ -32,20 +38,36 @@ class ThemeSwitcher {
       if (this.imgCardCerebro2) this.imgCardCerebro2.src = '../assets/cerebro2_branco.svg';
       if (this.imgCardBot) this.imgCardBot.src = '../assets/bot_branco.svg';
       if (this.imgGenovaNavbar) this.imgGenovaNavbar.src = '../assets/genova_branco.svg';
-      if (this.imgGenovaProfile) this.imgGenovaProfile.src = '../assets/genova_branco.svg'; // ✅ troca no profile
+      if (this.imgGenovaProfile) this.imgGenovaProfile.src = '../assets/genova_branco.svg';
       if (this.titulo) this.titulo.style.color = 'white';
       if (this.tituloSlideTablet) this.tituloSlideTablet.style.color = 'white';
       if (this.paragrafoSlideTablet) this.paragrafoSlideTablet.style.color = 'white';
+      if (this.imgHomePage) this.imgHomePage.src = '../assets/home_branco.svg.png';
+      if (this.imgChatGenova) this.imgChatGenova.src = '../assets/chat_branco.svg.png';
+      if (this.imgOpenSupport) this.imgOpenSupport.src = '../assets/support_branco.svg.png';
+      if (this.imgMyProfile) this.imgMyProfile.src = '../assets/profile_branco.svg.png';
+      if (this.imgHistoryIA) this.imgHistoryIA.src = '../assets/clipboard_ia_branco.svg.png';
+      if (this.imgHistoryChamado) this.imgHistoryChamado.src = '../assets/clipboard_chamado_branco.svg.png';
+      if (this.imgFaleConosco) this.imgFaleConosco.src = '../assets/telephone_branco.svg.png';
+      if (this.imgTheme) this.imgTheme.src = '../assets/theme_branco.svg.png';
     } else {
       if (this.imgGenova) this.imgGenova.src = '../assets/genova.svg';
       if (this.imgCardCerebro) this.imgCardCerebro.src = '../assets/cerebro.svg';
       if (this.imgCardCerebro2) this.imgCardCerebro2.src = '../assets/cerebro2.svg';
       if (this.imgCardBot) this.imgCardBot.src = '../assets/bot.svg';
       if (this.imgGenovaNavbar) this.imgGenovaNavbar.src = '../assets/genova.svg';
-      if (this.imgGenovaProfile) this.imgGenovaProfile.src = '../assets/genova.svg'; // ✅ volta normal no profile
+      if (this.imgGenovaProfile) this.imgGenovaProfile.src = '../assets/genova.svg';
       if (this.titulo) this.titulo.style.color = 'black';
       if (this.tituloSlideTablet) this.tituloSlideTablet.style.color = 'black';
       if (this.paragrafoSlideTablet) this.paragrafoSlideTablet.style.color = 'black';
+      if (this.imgHomePage) this.imgHomePage.src = '../assets/home.svg.png';
+      if (this.imgChatGenova) this.imgChatGenova.src = '../assets/chat.svg.png';
+      if (this.imgOpenSupport) this.imgOpenSupport.src = '../assets/support.svg.png';
+      if (this.imgMyProfile) this.imgMyProfile.src = '../assets/profile.svg.png';
+      if (this.imgHistoryIA) this.imgHistoryIA.src = '../assets/clipboard_ia.svg.png';
+      if (this.imgHistoryChamado) this.imgHistoryChamado.src = '../assets/clipboard_chamado.svg.png';
+      if (this.imgFaleConosco) this.imgFaleConosco.src = '../assets/telephone.svg.png';
+      if (this.imgTheme) this.imgTheme.src = '../assets/theme.svg.png';
     }
   }
 }
